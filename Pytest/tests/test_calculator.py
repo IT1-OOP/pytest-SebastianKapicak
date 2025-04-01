727896564f2a5641dcbac1e5d4c7755ae8e8a4ee
SebastianKapicak <sebastian.kapicak@student.vitkovickastredni.cz>
2025-04-01 10:05:07 +0200

    parametrized_done

--- Pytest/tests/test_calculator.py
+++ Pytest/tests/test_calculator.py
@@ -2,7 +2,19 @@ import pytest
 from src import calculator
 
 def test_add():
-    assert calculator.add(5,3) == 8
+    assert calculator.add(8,12) == 20
+    
+def test_add_wrong():
+    assert calculator.add_wrong(8,12) == 20
 
-def test_addwrong():
-    assert calculator.add_wrong(5, 3) ==8
+def test_substract():
+    assert calculator.substract(12,8) == 4
+    
+def test_multiply():
+    assert calculator.multiply(8,12) == 96
+    
+def test_multiply_wrong():
+    assert calculator.multiply_wrong(8,12) == 96
+    
+def test_divide():
+    assert calculator.divide(12,4) == 3
